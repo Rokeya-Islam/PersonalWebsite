247f291d471864042b88cb579e5d0ebc99e014e1
Rokeya-Islam <rokeyaislam90@gmail.com>
2023-07-29 11:53:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+          'primary-bg': '#F85559',
+          'title': '#121212cc',
+        }
+      },
+    },
+    plugins: [],
+  }
